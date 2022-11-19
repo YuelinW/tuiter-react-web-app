2737f4b57caf47c6a99686408a8a9b7158f337bd
Heyang Chen <yuelin@iMac.local>
2022-11-19 01:17:00 -0800

    updated

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -18,8 +18,8 @@ export const deleteTuit = async (tid) => {
   .delete(`${TUITS_API}/${tid}`)
   return response.data
 }
-// export const updateTuit = async (tuit) => {
-//   const response = await axios
-//   .put(`${TUITS_API}/${tuit._id}`, tuit);
-//   return tuit;
-// }
+export const updateTuit = async (tuit) => {
+  const response = await axios
+  .put(`${TUITS_API}/${tuit._id}`, tuit);
+  return response.data;
+}
